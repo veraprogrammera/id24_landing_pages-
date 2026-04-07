439e3d61967d08dd56d7b1d6715d0fc9fcb54d6d
Shahin Mehrabi <sharabi10@icloud.com>
2026-04-07 13:08:53 +0200

    Push sista

--- eleventy.config.js
+++ eleventy.config.js
@@ -3,8 +3,8 @@ module.exports = function (eleventyConfig) {
 
   return {
     dir: {
-      input: ".",
-      includes: "src/_includes",
+      input: "src",
+      includes: "_includes",
       output: "_site"
     }
   };
